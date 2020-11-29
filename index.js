696b14ef69bb18dbe3be5878c1e6ac3db2ade7a6
Pedro Cerna <pedro.d.cerna@gmail.com>
2020-11-28 20:52:01 -0600

    Need to figure out how to make heat maps with D3.js.

--- index.js
+++ index.js
@@ -7,18 +7,80 @@ const pullData = async (url) => {
 };
 
 const formatData = async (dataObject) => {
-    const data = await dataObject
-    console.log(data);
-}
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const data = await dataObject;
+  const { baseTemperature } = data;
+  return data.monthlyVariance.map((entry) => {
+    return {
+      baseTemperature,
+      monthName: monthNames[entry.month - 1],
+        ...entry,
+      newTemperature: +(baseTemperature + entry.variance).toFixed(2),
+    };
+  });
+};
 
 const createHeat = async () => {
-    const data = await formatData(pullData(dataURL))
-    console.log('hello world');
-    const tooltip = d3
-      .select('body')
-      .append('div')
-      .attr('id', 'tooltip')
-      .style('opacity', 0);
-}
+  const data = await formatData(pullData(dataURL));
+  console.log(data);
+  const margin = {
+    top: 50,
+    left: 50,
+    right: 50,
+    bottom: 50,
+  };
+  const content = document.querySelector('.content');
+  const svgContainer = document.querySelector('.svg-container');
+  const footer = document.querySelector('.footer');
+  const height = footer.offsetTop - svgContainer.offsetTop - margin.bottom;
+  const width = content.clientWidth;
+  const tooltip = d3
+    .select('body')
+    .append('div')
+    .attr('id', 'tooltip')
+    .style('opacity', 0);
+  const svg = d3
+    .select('.svg-container')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .attr('preserveAspectRatio', 'xMinYMin meet')
+    .classed('svg-content', true)
+    .style('background', 'white');
 
-createHeat()
+  console.log(data.map((entry) => entry.newTemperature));
+
+  const x = d3
+    .scaleBand()
+    .range([margin.left, width - margin.right])
+    .domain(data.map((entry) => entry.year));
+  const y = d3
+    .scaleBand()
+    .range([height - margin.bottom, margin.top])
+    .domain(data.map((entry) => entry.monthName))
+    .padding(0.05);
+
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${height - margin.bottom})`)
+    .call(d3.axisBottom(x).tickSizeOuter(0));
+  svg
+    .append('g')
+    .attr('transform', `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y).tickSizeOuter(0));
+};
+
+createHeat();
